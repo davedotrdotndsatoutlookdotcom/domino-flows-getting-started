9c3b5586762e24a128be7ca96de484833856e299
Dave Ryerson <dave.r.nds@outlook.com>
2026-04-22 23:14:50 +0000

    verify add.py

--- workflow.py
+++ workflow.py
@@ -4,24 +4,24 @@ from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask
 @workflow
 def simple_math_workflow(a: int, b: int) -> float:
 
+    # Create first task
     add_task = DominoJobTask(
-        name="Add numbers",
-        domino_job_config=DominoJobConfig(command="python add.py"),
-        inputs={"first_value": int, "second_value": int},
-        outputs={"sum": int},
+        name='Add numbers',
+        domino_job_config=DominoJobConfig(Command="python add.py"),
+        inputs={'first_value': int, 'second_value': int},
+        outputs={'sum': int},
         use_latest=True
     )
-
     sum = add_task(first_value=a, second_value=b)
 
+    # Create second task
     sqrt_task = DominoJobTask(
-        name="Square root",
-        domino_job_config=DominoJobConfig(command="python sqrt.py"),
-        inputs={"value": int},
-        outputs={"sqrt": float},
+        name='Square root',
+        domino_job_config=DominoJobConfig(Command="python sqrt.py"),
+        inputs={'value': int},
+        outputs={'sqrt': float},
         use_latest=True
     )
-
     sqrt = sqrt_task(value=sum)
 
     return sqrt
